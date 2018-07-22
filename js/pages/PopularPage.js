58f123051aed26ef1a1cddf2d0e26f4246089ef5
MaxYangyu <591282626@qq.com>
2018-07-22 15:34:32 +0800

    最热模块的开发

--- js/pages/PopularPage.js
+++ js/pages/PopularPage.js
@@ -5,30 +5,31 @@ import {
     StyleSheet,
     TextInput,
 } from 'react-native';
+import ScrollableTabView, {ScrollableTabBar} from 'react-native-scrollable-tab-view';
 import NavigationBar from '../common/navigationBar';
 import DataRepository from '../expand/dao/DataRepository'
 
-const URL = 'https://www.v2ex.com/api/nodes/show.json';
-const QUERY_STR = '?name='
+const URL = 'https://api.github.com/search/repositories?q=';
+const QUERY_STR = '&sort=stars';
 
 class PopularPage extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.dataRepository = new DataRepository();
         this.state = {
             result: ''
         }
     }
 
-    onLoad() {
-        let url = this.genUrl(this.text)
+    loadData() {
+        let url = this.genUrl(this.text);
         this.dataRepository.fetchNetRepository(url)
             .then(res => {
                 this.setState({
                     result: JSON.stringify(res)
                 })
             })
-            .catch(err=>{
+            .catch(err => {
                 this.setState({
                     result: JSON.stringify(err)
                 })
@@ -46,22 +47,65 @@ class PopularPage extends Component {
                     title={'最热'}
                     style={{backgroundColor: '#6495ED'}}
                 />
-                <Text
+                <ScrollableTabView
+                    tabBarPosition="top"
+                    renderTabBar={()=><ScrollableTabBar/>}
+                >
+                    <PopularTab tabLabel="Java">Java</PopularTab>
+                    <PopularTab tabLabel="iOS">IOS</PopularTab>
+                    <PopularTab tabLabel="Android">Android</PopularTab>
+                    <PopularTab tabLabel="JavaScript">JavaScript</PopularTab>
+                </ScrollableTabView>
+   {/*             <Text
                     onPress={() => {
-                        this.onLoad()
+                        this.loadData()
                     }}
                     style={styles.tips}>获取数据</Text>
                 <TextInput
                     autoCapitalize={'none'}
-                    style={{height: 40,borderWidth:1}}
+                    style={{height: 40, borderWidth: 1}}
                     onChangeText={text => this.text = text}/>
-                <Text style={{height:500}}>{this.state.result}</Text>
+                <Text style={{height: 500}}>{this.state.result}</Text>*/}
             </View>
         )
     }
 
 }
+class PopularTab extends  Component{
+    constructor(props){
+        super(props);
+        this.dataRepository = new DataRepository();
+        this.state = {
+            result: ''
+        }
+    }
+    componentDidMount(){
+        this.loadData()
+    }
+    loadData() {
+        let url = URL+this.props.tabLabel+QUERY_STR
+        this.dataRepository.fetchNetRepository(url)
+            .then(res => {
+                this.setState({
+                    result: JSON.stringify(res)
+                })
+            })
+            .catch(err => {
+                this.setState({
+                    result: JSON.stringify(err)
+                })
+            })
+    }
 
+
+    render(){
+        return(
+            <View>
+                <Text style={{height:600}}>{this.state.result}</Text>
+            </View>
+        )
+    }
+}
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -71,3 +115,4 @@ const styles = StyleSheet.create({
     }
 });
 export default PopularPage
+
